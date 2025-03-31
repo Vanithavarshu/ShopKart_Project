b9965fcb7c2c33cbc8ff723c09eee4bf6a059d1a
Vanithavarshu <dhivakarangappan@gmail.com>
2025-03-31 19:28:36 +0530

    Updated database settings

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -86,6 +86,11 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
+    'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
+}
+
+    
+"""DATABASES = {
     'default': {
         'default': dj_database_url.config(default=os.getenv('DATABASE_URL')),
         'ENGINE': 'django.db.backends.postgresql',
@@ -95,10 +100,8 @@ DATABASES = {
         'HOST': 'localhost',         
         'PORT': '5432',             
     }
-}
+}"""
 
-
-    
 
 
 # Password validation
