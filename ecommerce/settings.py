51e121b83ab0b8178c9b91b1a8e27834482e6156
Vanithavarshu <dhivakarangappan@gmail.com>
2025-03-31 16:02:19 +0530

    Updated database settings

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -85,6 +87,7 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
+        'default': dj_database_url.config(default=os.getenv('DATABASE_URL')),
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'ecommerce_project',  
         'USER': 'postgres',          
